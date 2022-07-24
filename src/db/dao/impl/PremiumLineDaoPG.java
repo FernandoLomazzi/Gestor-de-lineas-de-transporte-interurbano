937975a603e65fd00eabb93e014f37149a4531f4
AfterJack <luciorivera2000@gmail.com>
2022-07-24 17:51:18 -0300

    Preliminar finalizacion DAOS

---
+++ src/db/dao/impl/PremiumLineDaoPG.java
@@ -0,0 +1,30 @@
+package db.dao.impl;
+
+import db.dao.PremiumLineDao;
+import exceptions.AddFailException;
+import exceptions.DBConnectionException;
+import exceptions.DeleteFailException;
+import exceptions.ModifyFailException;
+import models.busline.PremiumLine;
+
+public class PremiumLineDaoPG implements PremiumLineDao {
+
+	@Override
+	public void addData(PremiumLine premiumLine) throws DBConnectionException, AddFailException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modifyData(PremiumLine premiumLine) throws DBConnectionException, ModifyFailException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteData(PremiumLine premiumLine) throws DBConnectionException, DeleteFailException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/db/dao/impl/PremiumLineServiceDAO.java
@@ -0,0 +1,5 @@
+package db.dao.impl;
+
+public interface PremiumLineServiceDAO {
+
+}
