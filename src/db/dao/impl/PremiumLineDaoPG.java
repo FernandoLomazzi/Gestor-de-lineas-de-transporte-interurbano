a4c71146370c2a3a234871622f8666fdc6e03aad
AfterJack <luciorivera2000@gmail.com>
2022-07-24 19:23:38 -0300

    Termine de escribir los DAOS, no se si funcionan

--- src/db/dao/impl/PremiumLineDaoPG.java
+++ src/db/dao/impl/PremiumLineDaoPG.java
@@ -8,10 +8,36 @@ import exceptions.ModifyFailException;
 import models.busline.PremiumLine;
 
 public class PremiumLineDaoPG implements PremiumLineDao {
+	private class PremiumLineServiceDaoPG {
+		//Le voy a dar una PremiumLine y me va hacer solo esta parte, lo hago asi para tener mejor organizacion
+		private static final String INSERT_SQL =
+				"INSERT INTO PremiumLineServices " + 
+				"(name_line, name_service) " +
+				"VALUES (?, ?);";
+		private static final String UPDATE_SQL = 
+				"UPDATE PremiumLineServices SET " + 
+				"name_line=?,name_service=? " +
+				"WHERE name_line=?;";
+		private static final String DELETE_SQL = 
+				"DELETE FROM PremiumLineServies " +
+				"WHERE name_line=?;";
+		
+		public void AddData()
+	}
+	private static final String INSERT_SQL_PREMIUM_LINE_SERVICE =
+			"INSERT INTO PremiumLine " + 
+			"(name,standing_capacity_percentage,standing_capacity) " +
+			"VALUES (?,?,?);";
+	private static final String UPDATE_SQL = 
+			"UPDATE CheapLine SET " + 
+			"name=?,standing_capacity_percentage=?,standing_capacity=? " +
+			"WHERE name=?;";
+	private static final String DELETE_SQL = 
+			"DELETE FROM CheapLine " +
+			"WHERE name=?;";
 
 	@Override
 	public void addData(PremiumLine premiumLine) throws DBConnectionException, AddFailException {
-		// TODO Auto-generated method stub
 		
 	}
 
