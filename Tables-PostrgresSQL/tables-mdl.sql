2c347239c7078f06f8d6f1085a4a9a39b6e3d41c
AfterJack <luciorivera2000@gmail.com>
2022-07-30 19:15:24 -0300

    Nuevos mdl de las tablas

--- Tables-PostrgresSQL/tables-mdl.sql
+++ Tables-PostrgresSQL/tables-mdl.sql
@@ -9,14 +9,27 @@ INSERT INTO Route VALUES
 (1,2,10.0),
 (1,3,7.5),
 (2,3,5.0),
+(3, 4, 4.0),
 (4,5,2.75);
 
 INSERT INTO BusLine VALUES
-('Linea1','0x16a216',10);
+('Linea1','0x16a216',10),
+('Linea2', '0x16a213', 20);
 
 INSERT INTO BusLineRoute VALUES
-('Linea1',1,2,10.0);
+('Linea1',1,2,10.0),
+('Linea2', 1, 2, 5),
+('Linea2', 2, 3, 4),
+('Linea2', 3, 4, 4);
 
 INSERT INTO BusLineStop VALUES
 ('Linea1',1,true),
-('Linea1',2,true);
+('Linea1',2,true),
+('Linea2', 1,true),
+('Linea2', 2,true),
+('Linea2', 3,true),
+('Linea2', 4,true);
+
+INSERT INTO CheapLine VALUES
+('Linea2', 0.12, 23);
+
