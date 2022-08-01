3f4fc14183ff30415028872877ba9166815c68c5
AfterJack <luciorivera2000@gmail.com>
2022-08-01 04:11:28 -0300

    Casi terminado delete y modify. Solo queda arreglar un bugsito el Dao de premiumLine. El de CheapLine funciona. Tambien faltan las alertas de operacion exitosa

--- src/controllers/line/showLineController.java
+++ src/controllers/line/showLineController.java
@@ -3,11 +3,15 @@ package controllers.line;
 import java.awt.Color;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
-import db.dao.BusLineDao;
-import db.dao.impl.BusLineDaoPG;
+import db.dao.CheapLineDao;
+import db.dao.PremiumLineDao;
+import db.dao.impl.CheapLineDaoPG;
+import db.dao.impl.PremiumLineDaoPG;
 import exceptions.DBConnectionException;
+import exceptions.DeleteFailException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,12 +20,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import managers.AlertManager;
 import managers.LineMapManager;
 import models.busline.BusLine;
 import models.busline.CheapLine;
@@ -80,7 +88,36 @@ public class showLineController implements Initializable {
 							stage.showAndWait();
     					}
     					else if (operation == avalibleOperations.DELETE) {
-    						//Falta
+    						BusLine busLine = row.getItem();
+    						Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    						alert.setHeaderText(null);
+    						alert.setTitle("Eliminación de la linea " + busLine.getName());
+    						alert.setContentText("¿Está seguro de que desea eliminar la linea " + busLine.getName() + "? Esta acción no se puede deshacer.");
+    						Optional<ButtonType> action = alert.showAndWait();
+    						if(action.get() == ButtonType.OK) {
+								if(busLine.getType() == "Económica") { 
+									CheapLineDao cheapLineDao = new CheapLineDaoPG();
+									try {
+										cheapLineDao.deleteData((CheapLine)busLine);
+									}
+									catch (DBConnectionException | DeleteFailException e) {
+										AlertManager.createAlert(AlertType.ERROR, "Error", e.getMessage());
+										return;
+									}
+								}
+								if(busLine.getType() == "Superior") {
+									PremiumLineDao premiumLineDao = new PremiumLineDaoPG();
+									try {
+										premiumLineDao.deleteData((PremiumLine)busLine);
+									}
+									catch (DBConnectionException | DeleteFailException e) {
+										AlertManager.createAlert(AlertType.ERROR, "Error", e.getMessage());
+										return;
+									}
+								}
+    						}
+    						lineMapManager.removeLine(busLine);
+    						((Stage)lineTable.getScene().getWindow()).close();
     					}
     				}
     				catch(IOException e) {
